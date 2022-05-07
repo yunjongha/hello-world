2dd6fb638d2abef6f3ddf835c4424ede759ae877
yunjongha <yunjongha@gmail.com>
2022-05-07 14:26:45 +0900

    condition test

--- identity.ts
+++ identity.ts
@@ -1,14 +1,20 @@
 
 class Identity {
-    nation: string;
-    name: string;
-    age: number;
+
+    constructor(public nation?: string, public name?: string, age?: number) {
+
+    }
+
 };
 
 var jh: Identity = new Identity();
 
-console.log(jh.nation);
-console.log(jh.name?.concat(' is a good man!'));
+try {
+    console.log(jh.nation);
+    console.log(jh.name?.concat(' is a good man!'));
+} catch (err) {
+    console.log('error: ', err);
+}
 
 
 
